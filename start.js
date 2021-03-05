e75f80576287902e13e0d824868819b26d0f7c6b
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2021-03-04 21:08:42 -0800

    changed question and options to be around the DIVEST acronym

--- start.js
+++ start.js
@@ -2,35 +2,36 @@ var startBtn = document.getElementById("start");
 var startPrompt = document.getElementById("start-prompt");
 var questionPrompt = document.getElementById("question-prompt");
 var timer = document.getElementById("timer-count");
-var questionTitle = document.getElementById("questions-text")
-var answerContainer = document.getElementById("answers")
-var currentIndex = 0
+var questionTitle = document.getElementById("questions-text");
+var answerContainer = document.getElementById("answers");
+var currentIndex = 0;
 
 
 var questions = [
     { 
-        question: "Is the sky blue?", 
-        options: ["True", "False"] 
-    },
-    
-    { 
-        question: "What do dogs eat?",
-        options: ["Dog food", "Cat food", "Pasta", "Pack rats"]
+        question: "What does 'Everything is not your fight' mean?", 
+        options: ["Don't be a Social Justice Warrior", "You can't stand up for yourself", "fighting is NOT feminine", "Make your attention/loyalty/anger expensive"] 
     },
     { 
-        question: "How long is class?",
-        options: ["3 1/2 hours", "2 hours", "14 hours", "4 1/2 hours"]
+        question: "which latin term is associated with the I in divest (INQUIRE)",
+        options: ["Que bonita!", "Qui bono?", "Question who benefits from your reaction", "Italiano!"]
     },
-  
     { 
-        question: "Is Tucson in California?",
-        options: ["True", "False"]
+        question: "The 'V' stands for...",
+        options: [" Verify information", "Vet everyone-including frineds and family", "Vested"]
     },
-
     { 
-        question: "How many pets do I have?",
-        options: ["3", "2", "0", "5"]    
+        question: "Examples of Disengaging from what doesn't serve you include: ",
+        options: ["Only Engaging in materials that are positive", "Disengage from social media that is draining and makes you feel bad", "Leave patterns of thinking that says that you are not valuable"]
     },
+    { 
+        question: "What does 'Self FIRST' mean?",
+        options: ["S is the 5th letter in the acronym", "Save yourself first"]    
+    },
+    {
+        question: "'Tell others who are ready' means",
+        options: ["Televize your movements to everyone","Move in silence","Test people to see if they are ready for the message before telling them about divestment"]
+    }
 ];
 // map of question with corresponding answers
 var answers = { 0:0 , 1:0, 2:3 , 3:2};
